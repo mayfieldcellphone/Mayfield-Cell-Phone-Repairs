321e4541869940ab98742d4f79a4acc3d0e1b0f3
Khalil Ahmad <149303486+mayfieldcellphone@users.noreply.github.com>
2026-04-26 07:08:43 +1000

    Sync changes from AI Studio

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import BrandPage from './pages/BrandPage';
+import ServicePage from './pages/ServicePage';
+import SuburbPage from './pages/SuburbPage';
+import BlogPage from './pages/BlogPage';
+import BlogPostPage from './pages/BlogPostPage';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import TermsOfService from './pages/TermsOfService';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="brand/:brandId" element={<BrandPage />} />
+          <Route path="service/:serviceId" element={<ServicePage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="blog/:slug" element={<BlogPostPage />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="terms-of-service" element={<TermsOfService />} />
+          <Route path=":serviceKeyword/:suburbId" element={<SuburbPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
